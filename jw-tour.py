ca0673e0977b6aea331af203510ed947d207ac4c
Victoria Sapronova <v.sapronova@gmail.com>
2018-03-30 20:15:07 -0400

    Added class JWBook

--- jw-tour.py
+++ jw-tour.py
@@ -1,11 +1,5 @@
 from selenium import webdriver
 
-def next_month(driver):
-    div = driver.find_element_by_class_name('right-arrow-wrap')
-    a = div.find_element_by_css_selector('a')
-    a.click()
-
-
 def get_month_days(days_divs):
     days_divs_text = [day.text for day in days_divs]
 
@@ -19,35 +13,44 @@ def get_month_days(days_divs):
     return month_days
 
 
-def current_month_dates(driver):
-    driver.find_element_by_class_name('availability-tile')
+class JWBookSite:
+    def __init__(self):
+        driver = webdriver.Chrome('/Users/victoria/Downloads/chromedriver')
+        driver.implicitly_wait(30)
+        driver.get('https://www.peek.com/s/9d74ebc1-e4fa-48de-917b-797d62c0ac27/rddM')
+        frame = driver.find_element_by_id('_pbf_1')
+        driver.switch_to.frame(frame)
+        self.driver = driver
 
-    month_divs = driver.find_elements_by_css_selector('div.month')
-    month_div = month_divs[0]
-    month_name = month_divs[0].find_element_by_class_name('month-title').text
-    days_divs = month_div.find_elements_by_css_selector('div.day')
-    month_days = get_month_days(days_divs)
+    def next_month(self):
+        div = self.driver.find_element_by_class_name('right-arrow-wrap')
+        a = div.find_element_by_css_selector('a')
+        a.click()
 
-    dates = []
-    for day in month_days:
-        cal_day = day.find_element_by_css_selector('div.calendar-day')
-        class_attr = cal_day.get_attribute('class')
-        if 'has-availability' in class_attr:
-            dates.append(int(day.text))
-    return (month_name, dates)
+    def current_month_dates(self):
+        self.driver.find_element_by_class_name('availability-tile')
+
+        month_divs = self.driver.find_elements_by_css_selector('div.month')
+        month_div = month_divs[0]
+        month_name = month_divs[0].find_element_by_class_name('month-title').text
+        days_divs = month_div.find_elements_by_css_selector('div.day')
+        month_days = get_month_days(days_divs)
+
+        dates = []
+        for day in month_days:
+            cal_day = day.find_element_by_css_selector('div.calendar-day')
+            class_attr = cal_day.get_attribute('class')
+            if 'has-availability' in class_attr:
+                dates.append(int(day.text))
+        return (month_name, dates)
 
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome('/Users/victoria/Downloads/chromedriver')
-    driver.implicitly_wait(30)
-    driver.get('https://www.peek.com/s/9d74ebc1-e4fa-48de-917b-797d62c0ac27/rddM')
-    frame = driver.find_element_by_id('_pbf_1')
-    driver.switch_to.frame(frame)
-
-    avail_dates = current_month_dates(driver)
+    site = JWBookSite()
+    avail_dates = site.current_month_dates()
     print(avail_dates)
-    next_month(driver)
-    avail_dates = current_month_dates(driver)
+    site.next_month()
+    avail_dates = site.current_month_dates()
     print(avail_dates)
 
 
