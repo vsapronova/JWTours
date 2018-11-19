e905f406f6dd713b9e7abe3f997b21c7fa1adc99
Victoria Sapronova <v.sapronova@gmail.com>
2018-11-19 18:46:08 -0500

    Validator of checking if an email is correct added

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, make_response
 from flask import request
 from datetime import datetime
 import db
@@ -10,26 +10,61 @@ from flask_cors import cross_origin
 
 app = Flask("HelloWorld")
 
+import re
+
+from typing import Dict
 
 @app.route('/<path:filename>')
 def static_file(filename):
     return send_from_directory('/Users/victoria/Projects/JWTours/frontend', filename)
 
 
+class Validator:
+    def __init__(self, request_data):
+        self.params_errors = []
+        self.request_data = request_data
+
+    def present(self, param_name):
+        if param_name not in self.request_data:
+            self.params_errors.append(param_name)
+        return param_name in self.request_data
+
+    def email(self):
+        if self.present('email'):
+            if not re.match(r'[^@]+@[^@]+\.[^@]+', self.request_data['email']):
+                self.params_errors.append('email')
+            else:
+                return self.request_data['email']
+        return None
+
+    def has_errors(self):
+        return len(self.params_errors) > 0
+
+    def error_response(self):
+        return jsonify({"error": "Bad parameters", "params_errors": self.params_errors}), 400
+
+
 @app.route('/api/create_request', methods=['POST'])
 @cross_origin()
 def create_request():
     #result = request.get_json()
-    result = json.loads(request.data)
-    email = result['email']
-    requested_date = datetime.strptime(result['requested_date'], "%Y-%m-%d")
+    request_data = json.loads(request.data)
+
+    validator = Validator(request_data)
+
+    email = validator.email()
+
+    if validator.has_errors():
+        return validator.error_response()
+
+    requested_date = datetime.strptime(request_data['requested_date'], "%Y-%m-%d")
     key = db.create_unique_key()
     req = Request(email, requested_date, key)
     db.insert_request(config, req)
     conf_link = "http://127.0.0.1:5000/confirm_request?key={0}&email={1}".format(key, email)
     subject = 'Please confirm your email'
     message = 'Please click on {} to confirm your email.'.format(conf_link)
-    sender.send_email_to_request(email, key, subject, message)
+    #sender.send_email_to_request(email, key, subject, message)
     return jsonify(success=True)
 
 
